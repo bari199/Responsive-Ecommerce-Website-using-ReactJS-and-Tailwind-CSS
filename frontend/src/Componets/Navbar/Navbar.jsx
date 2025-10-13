7c6265a0fe7907d926d10cfcf6ac8d5db5cd3055
moumita1530 <momtajbegum1530@gmail.com>
2025-10-13 11:12:04 -0700

    added navbar components

--- frontend/src/Componets/Navbar/Navbar.jsx
+++ frontend/src/Componets/Navbar/Navbar.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+const MenuLinks = [
+  {
+    id: 1,
+  },
+];
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div
+      className="bg-white dark:bg-gray-900 dark:text-white
+    duration-200 relative z-40"
+    >
+      <div className="py-4">
+        <div className="container">
+          <div>
+            <a
+              href="#"
+              className=" text-primary font-semibold tracking-widest text-2xl uppercase
+            sm:text-3xl"
+            >
+              Eshop
+            </a>
+            <div>
+              <ul>
+                {MenuLinks.map((data, index) => (
+                  <li>
+                    <a href=""></a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
